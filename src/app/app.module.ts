a787c4a1933df49d5d5803b371dfab1e621b7cb6
vishnu <vishnu@NTP-LAP-767.chn.nestgroup.net>
2022-12-06 19:35:49 +0530

    add navbar and readvalues from form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,39 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddempComponent } from './addemp/addemp.component';
 import { ViewEmpComponent } from './view-emp/view-emp.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { Route, RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+
+const myRoute:Routes=[
+  {
+    path:"",
+    component:AddempComponent
+  },
+  {
+  path:"viewemp",
+  component:ViewEmpComponent
+
+  }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     AddempComponent,
-    ViewEmpComponent
+    ViewEmpComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myRoute),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
